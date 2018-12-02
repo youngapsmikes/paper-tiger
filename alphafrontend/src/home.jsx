a7652b51f8cb794a966eceede404559270c39a0e
Michelle Yuen <mjyuen@princeton.edu>
2018-12-01 21:20:23 -0500

    added google auth

--- alphafrontend/src/home.jsx
+++ alphafrontend/src/home.jsx
@@ -1,11 +1,23 @@
 import React, { Component } from 'react';
-import PaperForm from './PaperForm.jsx';
 import FileUpload from './FileUpload.jsx';
-import { FilePond } from 'react-filepond';
 import 'filepond/dist/filepond.min.css';
 import { Modal } from 'react-bootstrap';
 import './home.css';
-import {Link } from 'react-router-dom';
+import { GoogleLogin } from 'react-google-login';
+
+const responseGoogle = (googleUser) => {
+  var profile = googleUser.getBasicProfile();
+        console.log("ID: " + profile.getId()); // Don't send this directly to your server!
+        console.log('Full Name: ' + profile.getName());
+        console.log('Given Name: ' + profile.getGivenName());
+        console.log('Family Name: ' + profile.getFamilyName());
+        console.log("Image URL: " + profile.getImageUrl());
+        console.log("Email: " + profile.getEmail());
+        // The ID token you need to pass to your backend:
+        var id_token = googleUser.getAuthResponse().id_token;
+        console.log("ID Token: " + id_token);
+
+}
 
 export default class Home extends Component {
     render() {
@@ -20,6 +32,12 @@ export default class Home extends Component {
             <div className = "fileupload">
             <FileUpload />
             </div>
+            <GoogleLogin
+              clientId="218437734175-0vhiaomko61rgce732icedd8ehfug697.apps.googleusercontent.com"
+              buttonText="Login"
+              onSuccess={responseGoogle}
+              onFailure={responseGoogle}
+            />
     
             </Modal.Body>
             </Modal.Dialog>
