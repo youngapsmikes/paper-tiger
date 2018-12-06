72eab9bd646fe009537a04092ccfd5bb3f28a255
qdonohue <43917220+qdonohue@users.noreply.github.com>
2018-12-06 18:01:30 -0500

    Added new logo and background image

--- alphafrontend/src/resultsPage.jsx
+++ alphafrontend/src/resultsPage.jsx
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import Results from "./results";
+import './resultsPage.css';
 
 
 
 export default class ResultsPage extends Component {
     render() {
         return (
-            <div>
+            <div className="temp">
                 <Results />
             </div>
         )
