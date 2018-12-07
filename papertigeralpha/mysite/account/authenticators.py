367dc4ca0b2ff2997183a426c17f2d1e8c97a35a
Bisrat Moges <bmoges@princeton.edu>
2018-12-07 17:49:54 -0500

    trivial changes

--- papertigeralpha/mysite/account/authenticators.py
+++ papertigeralpha/mysite/account/authenticators.py
@@ -11,17 +11,9 @@ class GoogleBackend(object):
 		    # Specify the CLIENT_ID of the app that accesses the backend:
 		    idinfo = id_token.verify_oauth2_token(token, requests.Request(), CLIENT_ID)
 
-		    # Or, if multiple clients access the backend server:
-		    # idinfo = id_token.verify_oauth2_token(token, requests.Request())
-		    # if idinfo['aud'] not in [CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]:
-		    #     raise ValueError('Could not verify audience.')
-
 		    if idinfo['iss'] not in ['accounts.google.com', 'https://accounts.google.com']:
 		        return None
 
-		    # If auth request is from a G Suite domain:
-		    # if idinfo['hd'] != GSUITE_DOMAIN_NAME:
-		    #     raise ValueError('Wrong hosted domain.')
 
 		    # ID token is valid. Get the user's Google Account ID from the decoded token.
 		    userid = idinfo['sub']
