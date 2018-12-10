4d258b3688bb7310b5872f99272c4ca28645aaaa
Michelle Yuen <mjyuen@princeton.edu>
2018-12-10 00:25:54 -0500

    added redirect upon successful login

--- alphafrontend/src/LoginPage.jsx
+++ alphafrontend/src/LoginPage.jsx
@@ -3,35 +3,45 @@ import { Modal } from 'react-bootstrap';
 import './home.css';
 import { GoogleLogin } from 'react-google-login';
 import {withRouter} from 'react-router-dom';
-
-
-const responseGoogle = (googleUser) => {
-  var profile = googleUser.getBasicProfile();
-        console.log("ID: " + profile.getId()); // Don't send this directly to your server!
-        console.log('Full Name: ' + profile.getName());
-        console.log('Given Name: ' + profile.getGivenName());
-        console.log('Family Name: ' + profile.getFamilyName());
-        console.log("Image URL: " + profile.getImageUrl());
-        console.log("Email: " + profile.getEmail());
-        // The ID token you need to pass to your backend:
-        var id_token = googleUser.getAuthResponse().id_token;
-        // Sending the token to the backend
-        console.log("ID Token: " + id_token);
-        var xhr = new XMLHttpRequest();
-        xhr.open('POST', '/account/login/');
-        xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-        xhr.onload = function() {
-          console.log('Signed in as: ' + xhr.responseText);
-        };
-        xhr.send('idtoken=' + id_token);
-
-        this.props.history.push('/home');
-
-
-}
+import { Redirect } from 'react-router';
 
 class LoginPage extends Component {
+  constructor(props) {
+    super(props);
+    this.responseGoogle = this.responseGoogle.bind(this);
+  }
+  responseGoogle(googleUser) {
+    var profile = googleUser.getBasicProfile();
+          console.log("ID: " + profile.getId()); // Don't send this directly to your server!
+          console.log('Full Name: ' + profile.getName());
+          console.log('Given Name: ' + profile.getGivenName());
+          console.log('Family Name: ' + profile.getFamilyName());
+          console.log("Image URL: " + profile.getImageUrl());
+          console.log("Email: " + profile.getEmail());
+          // The ID token you need to pass to your backend:
+          var id_token = googleUser.getAuthResponse().id_token;
+          // Sending the token to the backend
+          console.log("ID Token: " + id_token);
+          var xhr = new XMLHttpRequest();
+          xhr.open('POST', '/account/login/');
+          xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+          xhr.onload = function() {
+            console.log('Signed in as: ' + xhr.responseText);
+          };
+          xhr.send('idtoken=' + id_token);
+          console.log(this);
+          this.setState({redirect: true});
+  
+  }
+      state = {
+      redirect: false
+    }
+
     render() {
+      const { redirect } = this.state;
+      if (redirect) {
+        return <Redirect to='/home'/>;
+      }
         return (
           <div className="toplevel">
             <Modal.Dialog>
@@ -42,8 +52,8 @@ class LoginPage extends Component {
             <GoogleLogin
               clientId="218437734175-0vhiaomko61rgce732icedd8ehfug697.apps.googleusercontent.com"
               buttonText="Login"
-              onSuccess={responseGoogle}
-              onFailure={responseGoogle}
+              onSuccess={this.responseGoogle}
+              onFailure={this.responseGoogle}
             />
     
             </Modal.Body>
