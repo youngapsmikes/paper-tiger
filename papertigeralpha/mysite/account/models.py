18724746f3e48e7195289eb9565c48f8daed9911
Bisrat Moges <bmoges@princeton.edu>
2018-12-11 21:50:05 -0500

    Incomplete

--- papertigeralpha/mysite/account/models.py
+++ papertigeralpha/mysite/account/models.py
@@ -3,20 +3,21 @@ from django.contrib.auth.models import User
 from ML import recommend
 
 
-
-# Model to represent a single document 
+# Model to represent a single document
 class Paper(models.Model):
-	title = models.CharField(max_length = 200)
-	author = models.CharField(max_length = 200)
-	abstract = models.TextField()
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=200)
+    abstract = models.TextField()
 
-# Assocaite an id with multiple papers 
+
+# Assocaite an id with multiple papers
 class Project(models.Model):
-	pid = models.IntegerField() #Shoudl this be unique?
-	project_papers = models.ManyToManyField(Paper)
+    pid = models.IntegerField()  # Shoudl this be unique?
+    project_papers = models.ManyToManyField(Paper)
+
 
 # Base class that contains additonal information about a user
 class Researcher(models.Model):
-	user = models.OneToOneField(User, on_delete = models.CASCADE)
-	projects = models.ManyToManyField(Project)
-	pton_researcher = models.BooleanField(default = True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    projects = models.ManyToManyField(Project)
+    pton_researcher = models.BooleanField(default=True)
--- papertigeralpha/mysite/account/views.py
+++ papertigeralpha/mysite/account/views.py
@@ -5,13 +5,15 @@ from django.contrib.auth import authenticate, login
 from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth import authenticate, login
 
-@csrf_exempt 
+
+@csrf_exempt
 def user_login(request):
-	if request.method == 'POST':
-		token = request.POST.get('idtoken')
-		user = authenticate(request, token = token, CLIENT_ID = "218437734175-0vhiaomko61rgce732icedd8ehfug697.apps.googleusercontent.com")
-		if user is None :
-			pass
-		else:
-			login(request, user) 
-		return HttpResponse("LOGIN PROCESS HAS FINISHED")
+    if request.method == 'POST':
+        token = request.POST.get('idtoken')
+        user = authenticate(request, token=token,
+                            CLIENT_ID="218437734175-0vhiaomko61rgce732icedd8ehfug697.apps.googleusercontent.com")
+        if user is None:
+            pass
+        else:
+            login(request, user)
+        return HttpResponse("LOGIN PROCESS HAS FINISHED")
