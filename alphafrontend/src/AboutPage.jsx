541ba979d5450d80ec108d11d9c1a95ce4347732
youngapsmikes <michael.yifan.li@gmail.com>
2018-12-13 11:02:54 -0500

    Update AboutPage.jsx

--- alphafrontend/src/AboutPage.jsx
+++ alphafrontend/src/AboutPage.jsx
@@ -14,8 +14,8 @@ class AboutPage extends Component {
           </header>
           <div className="toplevel">
             <Jumbotron fluid>
-                <h1 className="display-1">Fluid jumbotron</h1>
-          <p className="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
+                <h1 className="display-1">Behind the Scenes</h1>
+          <p className="lead">How does paper tiger return recommendations? We use a machine learning algorithm called Latent Dirichlet Allocation (LDA). LDA assumes that documents are generated in the following way. Each document has two corresponding weighted dies. One die captures the likelihoods of various topics. For news, these topics could be categories like politics, culture, or science. Every topic also has a corresponding word die which captures the likelihoods of different words under a given topic. For example, words like "fake news" might be more common under a politics topic. By assuming this process for generating documents, LDA can then learn topics from a set of documents in an unsupervised way. How does this inference procedure work? We'll have to be hand-wavy here. Basically, we start off with effectively random assignments of words to topics. However, by examining the co-occurences of words between different documents (global properties) as well the types of words which dominate a document (local properties) we can discover the topics underlying a set of documents. </p>
             </Jumbotron>
             </div>
             </div>
