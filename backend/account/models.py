022ec6b8522a4536a900291850567921c9c31eff
Bisrat Moges <bmoges@princeton.edu>
2018-12-16 12:40:54 -0800

    test code

---
+++ backend/account/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+from django.contrib.auth.models import User
+from ML import recommend
+
+
+# Model to represent a single document
+class Paper(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=200)
+    abstract = models.TextField()
+
+
+# Assocaite an id with multiple papers
+class Project(models.Model):
+    pid = models.IntegerField()  # Shoudl this be unique?
+    project_name = models.CharField(max_length = 200)
+    project_papers = models.ManyToManyField(Paper)
+    
+
+# Base class that contains additonal information about a user
+class Researcher(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    projects = models.ManyToManyField(Project)
+    pton_researcher = models.BooleanField(default=True)
+    max_id = models.IntegerField(default=1)
---
+++ backend/account/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth import authenticate, login
+
+
+@csrf_exempt
+def user_login(request):
+    if request.method == 'POST':
+        token = request.POST.get('idtoken')
+        user = authenticate(request, token=token,
+                            CLIENT_ID="218437734175-0vhiaomko61rgce732icedd8ehfug697.apps.googleusercontent.com")
+        if user is None:
+        	# Frontend will deal with unauthenticated users 
+            pass
+        else:
+            login(request, user)
+        return HttpResponse("LOGIN PROCESS HAS FINISHED")
