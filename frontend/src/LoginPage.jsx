390361e727f535d63b105cf81b6cbf7ac5df7bb6
Bisrat Moges <bmoges@princeton.edu>
2018-12-23 01:29:04 -0800

    navbar fixed

--- frontend/src/LoginPage.jsx
+++ frontend/src/LoginPage.jsx
@@ -6,11 +6,15 @@ import {
 } from 'reactstrap';
 import { ListGroup, ListGroupItem } from 'reactstrap';
 import { Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap'
 import { Jumbotron} from 'reactstrap';
 import { GoogleLogin } from 'react-google-login';
 import {withRouter} from 'react-router-dom';
 import { Redirect } from 'react-router';
 import paperTigerlogofinal from './paperTigerlogofinal.png'
+
+
+import './home.css';
 
 
 class LoginPage extends Component {
@@ -58,35 +62,49 @@ class LoginPage extends Component {
       }
         return (
           <Container>
-          <Row>
-          <Col sm="12" md={{ size: 6, offset: 3 }}>
-            <div>
-              <ModalBody>
-                <img src={paperTigerlogofinal} height="227.2" width="320"/>
-                <Col sm="12" md={{ size: 6, offset: 3 }}>
-                <ModalFooter>
-                <GoogleLogin
-                  clientId="218437734175-0vhiaomko61rgce732icedd8ehfug697.apps.googleusercontent.com"
-                  buttonText="Login"
-                  theme="dark"
-                  onSuccess={this.responseGoogle}
-                  onFailure={this.responseGoogle}
-                />
-                </ModalFooter>
-                </Col>
-              </ModalBody>
-            </div>
-          </Col>
-          </Row>
+            <Row>
+                <div>
+                  <Navbar color="primary" fixed="top" light expand="md">
+                  <NavbarBrand> Paper Tiger </NavbarBrand>
+                    <Nav className="ml-auto" navbar>
+                    <NavItem>
+                      <NavLink>
+                        How it works
+                      </NavLink>
+                    </NavItem>
+                    </Nav>
+                  </Navbar>
+                </div>
+            </Row>
 
-          <Row>
-            <Col>
-              <Jumbotron fluid>
-                <h1 className="display-1">Behind the Machine Learning</h1>
-                <p className="lead">How does paper tiger return recommendations? We use a machine learning algorithm called Latent Dirichlet Allocation (LDA). LDA assumes that documents are generated in the following way. Each document has two corresponding weighted dice. One dice captures the probabilities of various topics. For news, these topics could be categories like politics, culture, or science. Every topic also has a corresponding word dice which captures the probabilities of different words under a given topic. For example, words like "fake news" might be more common under a "politics" topic than a "sports" topic. By assuming this process for generating documents, LDA can then learn topics from a set of documents in an unsupervised way. How does this inference procedure work? We'll have to be hand-wavy here. Basically, we start off with random assignments of words to topics. By examining the co-occurences of words throughout different documents as well the types of words which appear frequently within a particular document (local properties) we can discover the topics underlying a set of documents. </p>
-              </Jumbotron>
-            </Col>
-          </Row>
+            <Row>
+              <Col sm="12" md={{ size: 6, offset: 3 }}>
+                <div>                  
+                  <ModalBody>
+                    <img src={paperTigerlogofinal} height="227.2" width="320"/>
+                    <Col sm="12" md={{ size: 6, offset: 3 }}>
+                    
+                    <GoogleLogin
+                      clientId="218437734175-0vhiaomko61rgce732icedd8ehfug697.apps.googleusercontent.com"
+                      buttonText="Login"
+                      theme="dark"
+                      onSuccess={this.responseGoogle}
+                      onFailure={this.responseGoogle}
+                    />
+                    </Col>
+                  </ModalBody>
+                </div>
+              </Col>
+            </Row>
+
+            <Row>
+              <Col>
+                <Jumbotron>
+                  <h1 className="display-1">Behind the Machine Learning</h1>
+                  <p className="lead">How does paper tiger return recommendations? We use a machine learning algorithm called Latent Dirichlet Allocation (LDA). LDA assumes that documents are generated in the following way. Each document has two corresponding weighted dice. One dice captures the probabilities of various topics. For news, these topics could be categories like politics, culture, or science. Every topic also has a corresponding word dice which captures the probabilities of different words under a given topic. For example, words like "fake news" might be more common under a "politics" topic than a "sports" topic. By assuming this process for generating documents, LDA can then learn topics from a set of documents in an unsupervised way. How does this inference procedure work? We'll have to be hand-wavy here. Basically, we start off with random assignments of words to topics. By examining the co-occurences of words throughout different documents as well the types of words which appear frequently within a particular document (local properties) we can discover the topics underlying a set of documents. </p>
+                </Jumbotron>
+              </Col>
+            </Row>
           </Container>
         );
       }
