3009f875d01f9469ad79b6ebc1bf8fc3625b96d0
youngapsmikes <michael.yifan.li@gmail.com>
2018-12-22 21:10:45 -0500

    fixed error with returning wrong authors

--- backend/ML/recommend.py
+++ backend/ML/recommend.py
@@ -7,7 +7,10 @@ from django.conf import settings
 from scipy import spatial
 
 
-def recommend_lda(model, lda_X, tf_article, papers, authors):
+def getAuthors(paper_ids, authors, paper_authors):
+    return [" ".join(authors[authors.id.isin(paper_authors[paper_authors['paper_id'] == paper_id]['author_id'].values)].name.values) for paper_id in paper_ids] 
+
+def recommend_lda(model, lda_X, tf_article, papers, authors, paper_authors):
     dists = np.zeros((lda_X.shape[0],))
     article = model.transform(tf_article)
     
@@ -15,7 +18,9 @@ def recommend_lda(model, lda_X, tf_article, papers, authors):
         dists[idx] = np.linalg.norm(row-article)
     index = list(np.argsort(dists)[1:20])
     topic_vecs = list(lda_X[np.argsort(dists)[1:20]])
-    authors = list(authors[authors['id'].isin(index)]['name'])
+    # authors = list(authors[authors['id'].isin(index)]['name'])
+    paper_ids = papers.iloc[index].id.values
+    authors = getAuthors(paper_ids, authors, paper_authors)
     return list(zip(list(papers['title'][index]), topic_vecs, authors))
     
 def generate_Explanation(inputs, results, pdf_names):
@@ -34,19 +39,20 @@ def recommendMain(pdf_list, pdf_names):
 	cwd = os.path.join(str(settings.BASE_DIR), "ML")
 	print("cwd is: ", cwd)
 	os.chdir(cwd)
-	
+
 	## load in the models 
 	lda = load('lda_model.joblib') 
 	tf_vectorizer = load('tf_vectorizer.joblib')
 	lda_X = load('lda_X.joblib')
 	papers = pd.read_csv('papers.csv')
 	authors = pd.read_csv('authors.csv')
+	paper_authors = pd.read_csv('paper_authors.csv')
 
 	text = "".join(pdf_list)
 	combined_tfidf = tf_vectorizer.transform([text])
 
 	# vectorize all the papers associated with a project
 	separated_tfidf = list(map(lambda text: lda.transform(tf_vectorizer.transform([text]))[0], pdf_list))
-	recommendations = recommend_lda(lda, lda_X, combined_tfidf, papers, authors)
+	recommendations = recommend_lda(lda, lda_X, combined_tfidf, papers, authors, paper_authors)
 
 	return generate_Explanation(separated_tfidf, recommendations, pdf_names)
