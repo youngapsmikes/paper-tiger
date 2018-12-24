1724c358db9e745b91403a8633de83aeb57d62a4
Bisrat Moges <bmoges@princeton.edu>
2018-12-24 11:27:12 -0800

    update

--- frontend/src/LoginPage.jsx
+++ frontend/src/LoginPage.jsx
@@ -61,25 +61,32 @@ class LoginPage extends Component {
         return <Redirect to={newTo}/>;
       }
         return (
+          <React.Fragment>
+          <div>
           <Container>
+            {/* ----- Navigation Bar ----- */}
             <Row>
                 <div>
-                  <Navbar color="primary" fixed="top" light expand="md">
+                  <Navbar color="light" fixed="top" light expand="md">
                   <NavbarBrand> Paper Tiger </NavbarBrand>
                     <Nav className="ml-auto" navbar>
                     <NavItem>
                       <NavLink>
-                        How it works
+                        HOW IT WORKS
                       </NavLink>
                     </NavItem>
                     </Nav>
                   </Navbar>
                 </div>
             </Row>
+            </Container>
+            </div>
 
+            <div>
+            <Container>
+            {/* ----- Google Sign-in ----- */}
             <Row>
               <Col sm="12" md={{ size: 6, offset: 3 }}>
-                <div>                  
                   <ModalBody>
                     <img src={paperTigerlogofinal} height="227.2" width="320"/>
                     <Col sm="12" md={{ size: 6, offset: 3 }}>
@@ -93,21 +100,38 @@ class LoginPage extends Component {
                     />
                     </Col>
                   </ModalBody>
-                </div>
               </Col>
             </Row>
+            </Container>
+            </div>
 
+            <div>
+            <Container>
             <Row>
               <Col>
                 <Jumbotron>
-                  <h1 className="display-1">Behind the Machine Learning</h1>
+                  <h1 className="display-3">Behind the Machine Learning</h1>
                   <p className="lead">How does paper tiger return recommendations? We use a machine learning algorithm called Latent Dirichlet Allocation (LDA). LDA assumes that documents are generated in the following way. Each document has two corresponding weighted dice. One dice captures the probabilities of various topics. For news, these topics could be categories like politics, culture, or science. Every topic also has a corresponding word dice which captures the probabilities of different words under a given topic. For example, words like "fake news" might be more common under a "politics" topic than a "sports" topic. By assuming this process for generating documents, LDA can then learn topics from a set of documents in an unsupervised way. How does this inference procedure work? We'll have to be hand-wavy here. Basically, we start off with random assignments of words to topics. By examining the co-occurences of words throughout different documents as well the types of words which appear frequently within a particular document (local properties) we can discover the topics underlying a set of documents. </p>
                 </Jumbotron>
               </Col>
             </Row>
-          </Container>
+            </Container>
+            </div>
+            
+            <Container>
+              <Row>
+                <Col>
+                <div>
+                  <p>
+                    Please Work
+                  </p>
+                </div>
+                </Col>
+              </Row>
+            </Container>
+
+          </React.Fragment>
         );
       }
 }
-
 export default withRouter(LoginPage);
