465490f1ee529c6fe4a4f56d27c44a645db683d6
Michelle Yuen <mjyuen@princeton.edu>
2019-01-08 22:24:39 -0500

    styled and changed content of about page

--- frontend/src/AboutPage.jsx
+++ frontend/src/AboutPage.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Jumbotron } from 'react-bootstrap';
+import { Media } from 'react-bootstrap';
 import './about.css';
 import {withRouter} from 'react-router-dom';
 import PaperTigerHeader from './PaperTigerHeader.js';
@@ -12,12 +12,18 @@ class AboutPage extends Component {
           <header>
           <PaperTigerHeader userID={this.props.match.params.userID} authPayload={this.props.authPayload}/>
           </header>
+          <Media className="about">
           <div className="toplevel">
-            <Jumbotron fluid>
-                <h1 className="display-4">Behind the Machine Learning</h1>
-          <p className="lead">How does paper tiger return recommendations? We use a machine learning algorithm called Latent Dirichlet Allocation (LDA). LDA assumes that documents are generated in the following way. Each document has two corresponding weighted dice. One dice captures the probabilities of various topics. For news, these topics could be categories like politics, culture, or science. Every topic also has a corresponding word dice which captures the probabilities of different words under a given topic. For example, words like "fake news" might be more common under a "politics" topic than a "sports" topic. By assuming this process for generating documents, LDA can then learn topics from a set of documents in an unsupervised way. How does this inference procedure work? We'll have to be hand-wavy here. Basically, we start off with random assignments of words to topics. By examining the co-occurences of words throughout different documents as well the types of words which appear frequently within a particular document (local properties) we can discover the topics underlying a set of documents. </p>
-            </Jumbotron>
+            <p className="title"><strong><span className="titleText">Behind the Machine Learning</span></strong></p>
+            <p className="question"><strong><span className="normalText">How does paper tiger return recommendations?</span></strong></p>
+            <p className="question"><span className="normalText">We use a machine learning algorithm called Latent Dirichlet Allocation (LDA). LDA assumes that documents are generated in the following way. Each document has two corresponding weighted dice. One dice captures the probabilities of various topics. For news, these topics could be categories like politics, culture, or science. Every topic also has a corresponding word dice which captures the probabilities of different words under a given topic. For example, words like "fake news" might be more common under a "politics" topic than a "sports" topic. By assuming this process for generating documents, LDA can then learn topics from a set of documents in an unsupervised way. </span></p>
+            <p className="question"><strong><span className="normalText">How does this inference procedure work?</span></strong></p>
+            <p className="question"><span className="normalText">We'll have to be hand-wavy here. Basically, we start off with random assignments of words to topics. By examining the co-occurences of words throughout different documents as well the types of words which appear frequently within a particular document (local properties) we can discover the topics underlying a set of documents.</span></p>
+            <p className="title"><strong><span className="titleText">Behind the Site</span></strong></p>
+            <p className="question"><span className="normalText">paper tiger is a COS333 project made by Quinn Donohue &rsquo;20, Michael Li &rsquo;20, Bisrat Moges &rsquo;20, and Michelle Yuen &rsquo;20, and advised by Jace Lu and Bob Dondero.</span></p>            
             </div>
+            </Media>
+
             </div>
         );
       }
